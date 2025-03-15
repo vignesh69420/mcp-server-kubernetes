1cb8e54eb507e46eae4b907304ae3b07320d3aa1
Suyog Sonwalkar <flux159@gmail.com>
2025-03-15 16:17:33 -0700

    Getting CI started
    Summary:
    
    Test Plan:

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vitest/config'
+import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
     // Set timeout to 120 seconds for all tests
-    testTimeout: 120000
+    testTimeout: 120000,
   },
-})
+});
