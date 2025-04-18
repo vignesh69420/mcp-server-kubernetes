debafa39a36ae0d8be4c284e90d66c615e471803
osw282 <25309418+osw282@users.noreply.github.com>
2025-04-18 15:29:50 +0100

    Describe node tool

--- src/index.ts
+++ src/index.ts
@@ -12,6 +12,7 @@ import { listCronJobs, listCronJobsSchema } from "./tools/list_cronjobs.js";
 import { describeCronJob, describeCronJobSchema } from "./tools/describe_cronjob.js";
 import { listJobs, listJobsSchema } from "./tools/list_jobs.js";
 import { getJobLogs, getJobLogsSchema } from "./tools/get_job_logs.js";
+import { describeNode, describeNodeSchema } from "./tools/describe_node.js";
 import {
   installHelmChart,
   installHelmChartSchema,
@@ -112,6 +113,7 @@ server.setRequestHandler(ListToolsRequestSchema, async () => {
     deleteNamespaceSchema,
     describeCronJobSchema,
     describePodSchema,
+    describeNodeSchema,
     describeDeploymentSchema,
     explainResourceSchema,
     getEventsSchema,
@@ -232,6 +234,16 @@ server.setRequestHandler(
 
         case "describe_pod": {
           return await describePod(
+            k8sManager,
+            input as {
+              name: string;
+              namespace: string;
+            }
+          );
+        }
+
+        case "describe_node": {
+          return await describeNode(
             k8sManager,
             input as {
               name: string;
